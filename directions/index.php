d7c863303da432ba9d51f2afcfdbf06acf6ac6e0
Avram Eisner <aeisner@mobiquityinc.com>
2015-11-27 20:46:56 -0800

    Updated link to google maps to use correct address for Golden Eye.

--- directions/index.php
+++ directions/index.php
@@ -16,7 +16,7 @@
         <div class="map col-sm-7 col-xs-12">
           <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d42177.07171396407!2d-122.91912943475651!3d48.6468338011978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x5485871e34e9507d%3A0xdff668f5e559eca4!2sFerry+Dock%2C+Eastsound%2C+WA+98245!3m2!1d48.597919999999995!2d-122.9440149!4m5!1s0x54858462bce1f06d%3A0x71903e09aa30e810!2s850+Point+Lawrence+Rd%2C+Olga%2C+WA+98279!3m2!1d48.624207999999996!2d-122.820488!5e0!3m2!1sen!2sus!4v1448685538103" frameborder="0" style="border:0" allowfullscreen></iframe>
           <br>
-          <a target="_blank" href="https://www.google.com/maps/dir/Ferry+Dock,+Eastsound,+WA+98245/895+Point+Lawrence+Rd,+Olga,+WA+98279/@48.6467588,-122.957659,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x5485871e34e9507d:0xdff668f5e559eca4!2m2!1d-122.9440149!2d48.59792!1m5!1m1!1s0x54858462890ddbbb:0x49c885ae04530ae3!2m2!1d-122.8199529!2d48.6262431">Link to Google Maps</a>
+          <a target="_blank" href="https://www.google.com/maps/dir/Ferry+Dock,+Eastsound,+WA+98245/850+Point+Lawrence+Rd,+Olga,+WA+98279/@48.6467588,-122.957659,12z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x5485871e34e9507d:0xdff668f5e559eca4!2m2!1d-122.9440149!2d48.59792!1m5!1m1!1s0x54858462890ddbbb:0x49c885ae04530ae3!2m2!1d-122.8199529!2d48.6262431">Link to Google Maps</a>
         </div>
         <div class="directions col-sm-5 col-xs-12">
           <ol>
