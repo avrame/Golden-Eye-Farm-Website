3e0b08926e58e281fcfaa90ff033354f0c2b3476
Avram Eisner <aeisner@mobiquityinc.com>
2016-04-04 21:45:01 -0700

    Added lightbox stylesheet to head.

--- _inc/head.php
+++ _inc/head.php
@@ -17,6 +17,8 @@
   <!-- Custom styles for this template -->
   <link href="http://<?= $_SERVER['HTTP_HOST'] ?>/_css/main.css" rel="stylesheet">
 
+  <link href="http://<?= $_SERVER['HTTP_HOST'] ?>/_css/lightbox.css" rel="stylesheet">
+
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
